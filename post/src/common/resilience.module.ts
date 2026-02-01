35e04f7d184c41935757b04f1d144915d9e5b450
bleidertcs <bleider.colina@tcsvzla.com>
2026-02-01 19:13:23 -0400

    refactor: remove legacy post service following migration to ecommerce microservices

--- post/src/common/resilience.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { CircuitBreakerService } from './services/circuit-breaker.service';
-
-@Global()
-@Module({
-    providers: [CircuitBreakerService],
-    exports: [CircuitBreakerService],
-})
-export class ResilienceModule {}
